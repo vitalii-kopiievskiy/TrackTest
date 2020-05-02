c484d59ebd2aedd571ad24b04ec5dc0d59018320
vitalii-kopiievskiy <vitalii.kopiievskiy@gmail.com>
2020-05-02 19:37:51 +0300

    clear start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-import { interval, Subscription } from "rxjs";
 
 @Component({
   selector: "app-root",
@@ -7,16 +6,5 @@ import { interval, Subscription } from "rxjs";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  sub: Subscription;
-  constructor() {
-    const intervalStream$ = interval(1000);
-    console.log(intervalStream$);
-    this.sub = intervalStream$.subscribe((value) => {
-      console.log(value);
-    });
-  }
-
-  stop() {
-    this.sub.unsubscribe();
-  }
+  constructor() {}
 }
