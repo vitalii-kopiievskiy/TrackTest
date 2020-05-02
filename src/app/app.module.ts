a0aba5d7e3def16387369e3592bc8b59d11226ea
vitalii-kopiievskiy <vitalii.kopiievskiy@gmail.com>
2020-05-02 19:52:26 +0300

    add components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { TracksComponent } from './tracks/tracks.component';
+import { TracksFormsComponent } from './tracks-forms/tracks-forms.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TracksComponent, TracksFormsComponent, MapComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/tracks-forms/tracks-forms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tracks-forms',
+  templateUrl: './tracks-forms.component.html',
+  styleUrls: ['./tracks-forms.component.scss']
+})
+export class TracksFormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
